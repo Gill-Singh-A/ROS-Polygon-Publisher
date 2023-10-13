580d50da3d586cd33a3878081c2bcb838097a4b1
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-13 13:21:04 +0530

    Implemented Empty Window using PyGame

--- src/polygon.py
+++ src/polygon.py
@@ -1,4 +1,26 @@
 #! /usr/bin/env python3
 
+import pygame, rospy
+from geometry_msgs.msg import Point32
+from geometry_msgs.msg import Polygon
+
+WIDTH, HEIGHT = 600, 600
+frequency = 10
+points = []
+
+polygon_publisher = rospy.Publisher("/polygon", Polygon, queue_size=10)
+
 if __name__ == "__main__":
-    pass
+    rospy.init_node("polygon")
+    rate = rospy.Rate(frequency)
+    pygame.init()
+    logo = pygame.image.load("assets/images/ariitk.jpg")
+    pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
+    pygame.display.set_caption("Polygon")
+    pygame.display.set_icon(logo)
+    running = True
+    while running and not rospy.is_shutdown():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                break
