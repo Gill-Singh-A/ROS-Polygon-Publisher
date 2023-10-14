e3abc25710d6b1d2f40379083605001171e5e06c
Gill-Singh-A <amansg22@iitk.ac.in>
2023-10-15 03:17:19 +0530

    Added Index of point in the z component of the 3D Points

--- src/polygon.py
+++ src/polygon.py
@@ -60,7 +60,7 @@ if __name__ == "__main__":
         screen.fill(background_color)
         ros_publisher_points = Polygon()
         for draw_point_index, point in enumerate(points):
-            ros_publisher_point = Point32(point[0], point[1], 0)
+            ros_publisher_point = Point32(point[0], point[1], draw_point_index)
             ros_publisher_points.points.append(ros_publisher_point)
             pygame.draw.line(screen, line_color, point, points[(draw_point_index+1)%len(points)], line_width)
         polygon_publisher.publish(ros_publisher_points)
