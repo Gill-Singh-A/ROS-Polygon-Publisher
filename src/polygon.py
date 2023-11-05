a6d68006e837c3ac553c6208d5a8962a45e55c5e
Gill-Singh-A <amansg22@iitk.ac.in>
2023-11-05 21:42:30 +0530

    Added Grid

--- src/polygon.py
+++ src/polygon.py
@@ -7,8 +7,15 @@ from geometry_msgs.msg import Polygon
 
 background_color = (0, 0, 0)
 line_color = (255, 255, 255)
+minor_line_color = (100, 100, 100)
+major_line_color = (200, 200, 200)
+font_color = (250, 250, 250)
 line_width = 5
+minor_line_width = 1
+major_line_width = 2
 WIDTH, HEIGHT = 600, 600
+major_x, minor_x, major_y, minor_y = 100, 10, 100, 10
+font_size = 20
 frequency = 10
 points = []
 
@@ -16,6 +23,26 @@ polygon_publisher = rospy.Publisher("/polygon", Polygon, queue_size=10)
 
 def distance(point_1, point_2):
     return math.sqrt((point_1[0]-point_2[0])**2+(point_1[1]-point_2[1])**2)
+
+def drawGrid(screen, font):
+    for x in range(0, WIDTH, minor_x):
+        pygame.draw.line(screen, minor_line_color, (x, 0), (x, HEIGHT), minor_line_width)
+    for x in range(0, WIDTH, major_x):
+        pygame.draw.line(screen, major_line_color, (x, 0), (x, HEIGHT), major_line_width)
+    for y in range(0, WIDTH, minor_y):
+        pygame.draw.line(screen, minor_line_color, (0, y), (WIDTH, y), minor_line_width)
+    for y in range(0, WIDTH, major_y):
+        pygame.draw.line(screen, major_line_color, (0, y), (WIDTH, y), major_line_width)
+    for x in range(0, WIDTH, major_x):
+        text = font.render(f"{x//major_x}", True, font_color, background_color)
+        textPos = text.get_rect()
+        textPos.center = (x + font_size//2, font_size // 2 + major_line_width)
+        screen.blit(text, textPos)
+    for y in range(0, WIDTH, major_y):
+        text = font.render(f"{y//major_y}", True, font_color, background_color)
+        textPos = text.get_rect()
+        textPos.center = (font_size//2, y + font_size // 2 + major_line_width)
+        screen.blit(text, textPos)
 
 if __name__ == "__main__":
     rospy.init_node("polygon")
@@ -25,6 +52,7 @@ if __name__ == "__main__":
     screen = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
     pygame.display.set_caption("Polygon")
     pygame.display.set_icon(logo)
+    font = pygame.font.Font('freesansbold.ttf', font_size)
     print("Left Click to Add a Point")
     print("Right Click to Delete the Nearest Point")
     print("Press Backspace on Keyboard to delete the Lastest Added Point")
@@ -58,6 +86,7 @@ if __name__ == "__main__":
                     if point_index != -1:
                         points.pop(point_index)
         screen.fill(background_color)
+        drawGrid(screen, font)
         ros_publisher_points = Polygon()
         for draw_point_index, point in enumerate(points):
             ros_publisher_point = Point32(point[0], point[1], draw_point_index)
